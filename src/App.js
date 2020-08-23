3de565c7c34d0ac5045ac934819be2664315fe5b
erdal-u <erdal.ustalar@gmail.com>
2020-08-23 13:14:43 +0200

    bild cv uppe och länk till mail sida klart

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import Erdalcv from './Erdalcv.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img src={Erdalcv}  alt="logo" />
+     
+      
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.google.com/intl/sv/gmail/about/#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Erdal.ustalar@gmail.com
         </a>
       </header>
     </div>
